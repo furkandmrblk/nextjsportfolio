1ec8afa81f18feb0997cb161d66f29978146520f
Furkan <ibrahimdmrblk@outlook.de>
2021-01-26 12:34:28 +0100

    changed favicon

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600&family=Racing+Sans+One&family=Spartan:wght@700;800;900&display=swap"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon-16x16.png" />
         <title>Ibrahim-Furkan Demirbilek - furkandmrblk | Landing Page</title>
       </Head>
       <GlobalContainer id="global">
